5171668b91fbb8c664c3be9aae486fd6896632bc
panjacob <s19615@pjwstk.edu.pl>
2021-10-01 17:19:22 +0200

    Added Login, Logout, LogoutAll via API

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
+from knox import views as knox_views
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.LoginAPI.as_view(), name='login'),
+    path('logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
     path('register/', views.RegisterAPI.as_view(), name='register'),
 ]
