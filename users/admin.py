4cb5f723bd48bd9bb3f6a7f4cf9fbe29068b1060
panjacob <s19615@pjwstk.edu.pl>
2021-10-13 19:13:59 +0200

    Added user to adminpanel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from users.models import UserExtended
+
+
+class AdminUserExtended(admin.ModelAdmin):
+    model = UserExtended
+    list_display = ['id', 'user_name', 'first_name']
+
+
+admin.site.register(UserExtended, AdminUserExtended)
