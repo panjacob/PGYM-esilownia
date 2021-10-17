8d4ba172f92663718cd2bf15867f26447468ff0e
panjacob <kwiatkowskijakub@protonmail.com>
2021-10-17 16:53:59 +0200

    Initialize app trainings

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'users',
     'dashboard',
+    'trainings',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
