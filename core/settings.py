039a6900c3efb70b9e62a04ba0f00a2504c11535
panjacob <kwiatkowskijakub@protonmail.com>
2021-10-20 18:57:56 +0200

    Added cors allow all for development

--- core/settings.py
+++ core/settings.py
@@ -32,27 +32,27 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-# CORS_ALLOWED_ORIGINS = ['*']
-
 INSTALLED_APPS = [
+    # Apps
     'users',
     'dashboard',
     'trainings',
+    # Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # rest-framework
+    # Rest
     'rest_framework',
     'rest_framework.authtoken',
-    # 'rest_framework_simplejwt.token_blacklist',
-    # 'corsheaders'
     # drf-social-oauth2
     'oauth2_provider',
     'social_django',
     'drf_social_oauth2',
+    # CORS
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -76,6 +76,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # cors
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -161,33 +163,9 @@ from django.core.files.storage import FileSystemStorage
 
 MEDIA_PHOTO_VIDEOS_PATH = FileSystemStorage(location='/media/photos_videos')
 
-# SIMPLE_JWT = {
-#     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-#     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
-#     'ROTATE_REFRESH_TOKENS': True,
-#     'BLACKLIST_AFTER_ROTATION': False,
-#     'UPDATE_LAST_LOGIN': False,
-#
-#     'ALGORITHM': 'HS256',
-#     'SIGNING_KEY': SECRET_KEY,
-#     'VERIFYING_KEY': None,
-#     'AUDIENCE': None,
-#     'ISSUER': None,
-#     'JWK_URL': None,
-#     'LEEWAY': 0,
-#
-#     'AUTH_HEADER_TYPES': ('Bearer', 'JWT'),
-#     'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
-#     'USER_ID_FIELD': 'id',
-#     'USER_ID_CLAIM': 'user_id',
-#     'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
-#
-#     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-#     'TOKEN_TYPE_CLAIM': 'token_type',
-#
-#     'JTI_CLAIM': 'jti',
-#
-#     'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-#     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
-#     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
-# }
+CORS_ORIGIN_ALLOW_ALL = True
+
+# On production
+# CORS_ORIGIN_WHITELIST = ('http://localhost:3000',)
+
+# CORS_ALLOWED_ORIGINS = ['*']
