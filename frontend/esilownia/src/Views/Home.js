fdade3de5f6e60ab472812b54cf68409fc091920
Dudziej <dudzik32@gmail.com>
2021-10-22 20:18:55 +0200

    Home karuzela

--- frontend/esilownia/src/Views/Home.js
+++ frontend/esilownia/src/Views/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Oferta_trener from "../components/Home/Oferta_trener"
+import Karuzela from "../components/Home/Carousel";
 
 function Home() {
   return (
@@ -10,7 +11,7 @@ function Home() {
           <div class="col-lg-7">
             <img
               class="img-fluid rounded mb-4 mb-lg-0"
-              src="https://riotopgym.pl/wp-content/uploads/2020/06/IMG-3651.jpg"
+              src="https://images.unsplash.com/photo-1571902943202-507ec2618e8f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZW1wdHklMjBneW18ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
               alt=""
             />
           </div>
@@ -26,23 +27,9 @@ function Home() {
         </div>
       </div>
       <div class="container text-center">
-        <h1 class="display-1">Zacznij swój trening!</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-           Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa.
-            Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit.
-             Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent.
-           In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac.</p>
-        <div class="container align-items-center my-5">
-          <div class="container-lg-7">
-              <img
-                class="img-fluid rounded mb-4 mb-lg-0"
-                src="https://riotopgym.pl/wp-content/uploads/2020/06/IMG-3651.jpg"
-                alt=""
-              />
-            </div>
-          </div>
-      </div>
-      <div class="container text-center">
+        <h1 class="display-1 pb-4">Zacznij swój trening!</h1>
+        <Karuzela></Karuzela>
+        <button class="btn btn-lg mt-4">Zapoznaj się z ofertą</button>
         <h1 class="display-1 pb-4">Jesteś trenerem?</h1>
         <h4 class="font-weight-light pb-4">Dołącz do naszego zespołu!</h4>
         <Oferta_trener></Oferta_trener>
