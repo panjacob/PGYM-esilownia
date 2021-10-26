c263adc5d787e5cddba4252fa544247af23f857b
panjacob <s19615@pjwstk.edu.pl>
2021-10-26 19:19:33 +0200

    Autoinitialization of admin and OAuth2 application esilownia

--- users/apps.py
+++ users/apps.py
@@ -10,10 +10,35 @@ class AppConfig(AppConfig):
         init_esilownia_application()
 
 
-def init_esilownia_application():
+def init_esilownia_application(
+        client_id='dFdnC978WJPKdx9efSKTFdym2LIQWkd8HXGMTpzs',
+        client_type='confidential',
+        client_secret='jjKsavkJG913gLTzWm4bcYjLWCdUUDapm7tuXElBji9FQuWckkty2RkqU0Z0rTIqiZw1UlQiKjMUG9a92whxOLCOZriszlHiNfIAELTp6zde31T0IgLKiRzOAzm4HihV',
+        skip_authorization=0,
+        authorization_grant_type='password',
+        name='esilownia'
+):
     try:
-        from
-        pass
+        from oauth2_provider.models import Application
+        user = create_admin_default()
+        app, created = Application.objects.get_or_create(id=1)
+        app.user = user
+        app.client_id = client_id
+        app.client_type = client_type
+        app.client_secret = client_secret
+        app.skip_authorization = skip_authorization
+        app.authorization_grant_type = authorization_grant_type
+        app.name = name
+        app.save()
 
     except Exception as e:
-        pass
+        print('Application exist, its OK', e)
+
+
+def create_admin_default():
+    from users.models import UserExtended
+    if not UserExtended.objects.filter(id=1).exists():
+        return UserExtended.objects.create_superuser(id=1, username='admin', email='admin@admin.pl',
+                                                     first_name='admin', password='admin')
+    else:
+        return UserExtended.objects.get(id=1)
