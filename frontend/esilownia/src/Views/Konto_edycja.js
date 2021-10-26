a7d8981f3ae90123ddca508bab361e14974f3561
C-h-e-s-h-i-r-e <aleksanderbober1999@gmail.com>
2021-10-26 16:09:22 +0200

    Naprawa Edit User i dodanie Change Password

--- frontend/esilownia/src/Views/Konto_edycja.js
+++ frontend/esilownia/src/Views/Konto_edycja.js
@@ -9,42 +9,109 @@ function Konto() {
     const [username, setUsername] = useState("");
     const [firstname, setFirstname] = useState("");
     const [lastname, setLastname] = useState("");
-    const [password, setPassword] = useState("");
+    const [oldpassword, setOldPassword] = useState("");
+    const [newpassword, setNewPassword] = useState("");
+
+    async function postData(url = '', data = {}) {
+        const response = await fetch(url, {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer' + " " + localStorage.getItem('access_token')
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(data)
+        });
+        return response.json();
+    }
 
     const handleSubmitData = (e) => {
         e.preventDefault();
-        console.log(username)
-        console.log(firstname)
-        console.log(lastname)
-        console.log(email)
+        //console.log(username)
+        //console.log(firstname)
+        //console.log(lastname)
+        //console.log(email)
 
-        axiosInstance
-            .post(`users/edit/`, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer' + " " + localStorage.getItem('access_token')
-                },
-                username: username,
-                first_name: firstname,
-                last_name: lastname,
-                email: email
-
-            })
-            .then((res) => {
-                console.log(res)
-                console.log(res.data)
-
-                setEmail(res.data.email)
-                setUsername(res.data.username)
-                setFirstname(res.data.first_name)
-                setLastname(res.data.last_name)
-
+        postData('http://127.0.0.1:8000/users/edit/', {
+                    username: username,
+                    first_name: firstname,
+                    last_name: lastname,
+                    email: email })
+            .then(data => {
+                //console.log(data);
+                window.location.reload();
             });
+
+
+
+
+        // AXIOS KOD 401 UNAUTHORIZED - POWOD NIEWIADOMY
+
+        // axiosInstance
+        //     .post(`auth/token/`, {
+        //         grant_type: 'refresh_token',
+        //         refresh_token : localStorage.getItem('refresh_token'),
+        //         client_id: 'TUz2wd25Z9hfRbOUr9z3CFEKAc42hJrjsz57sMt6',
+        //         client_secret:
+        //             'QpMCaevBW6VRJ42wtJ1Cgqitz0aVuBMJRQFgULMTGYievg572RVlcQoTD6xtaVf4mL6K38Df6tcazzfsxMfaDTEjzbH343kFCItJfJKEa2bcjL0ukufLOsfQCAFx3hTR',
+        //
+        //     })
+        //     .then((res) => {
+        //         console.log(res)
+        //         console.log(res.data)
+        //
+        //         localStorage.setItem('access_token', res.data.access_token);
+        //         localStorage.setItem('refresh_token', res.data.refresh_token);
+        //
+        //     });
+        //
+        // const dataToSend = { username: username,
+        //                 first_name: firstname,
+        //                 last_name: lastname,
+        //                 email: email };
+        // axiosInstance
+        //     .post(`users/edit/`, {
+        //                 method: 'POST',
+        //                 mode: 'cors',
+        //                 cache: 'no-cache',
+        //                 credentials: 'same-origin',
+        //                 headers: {
+        //                     'Content-Type': 'application/json',
+        //                     'Authorization': 'Bearer' + " " + localStorage.getItem('access_token')
+        //                 },
+        //                 redirect: 'follow',
+        //                 referrerPolicy: 'no-referrer',
+        //                 body: JSON.stringify(dataToSend)
+        //
+        //     })
+        //     .then((res) => {
+        //         console.log(res)
+        //         console.log(res.data)
+        //
+        //         setEmail(res.data.email)
+        //         setUsername(res.data.username)
+        //         setFirstname(res.data.first_name)
+        //         setLastname(res.data.last_name)
+        //
+        //     });
     };
 
     const handleSubmitPass = (e) => {
         e.preventDefault();
-        console.log(password)
+        //console.log(oldpassword)
+        //console.log(newpassword)
+
+        postData('http://127.0.0.1:8000/users/change_password/', {
+            old_password: oldpassword,
+            new_password: newpassword })
+            .then(data => {
+                //console.log(data);
+                //window.location.reload();
+            });
     };
 
     useEffect(() => {
@@ -57,8 +124,8 @@ function Konto() {
                 }
             })
             .then((res) => {
-                console.log(res)
-                console.log(res.data)
+                //console.log(res)
+                //console.log(res.data)
 
                 setEmail(res.data.email)
                 setUsername(res.data.username)
@@ -132,11 +199,21 @@ function Konto() {
                         <div className="card-body">
                             <div className="row">
                                 <div className="col-sm-3">
-                                    <h6 className="mb-0">Hasło</h6>
+                                    <h6 className="mb-0">Stare hasło</h6>
                                 </div>
                                 <div className="col-sm-9 text-secondary">
                                     <input type="password" className="form-control form-control-sm"
-                                           onChange={(e) => setPassword(e.target.value)}/>
+                                           onChange={(e) => setOldPassword(e.target.value)}/>
+                                </div>
+                            </div>
+                            <hr></hr>
+                            <div className="row">
+                                <div className="col-sm-3">
+                                    <h6 className="mb-0">Nowe Hasło</h6>
+                                </div>
+                                <div className="col-sm-9 text-secondary">
+                                    <input type="password" className="form-control form-control-sm"
+                                           onChange={(e) => setNewPassword(e.target.value)}/>
                                 </div>
                             </div>
                             <hr></hr>
