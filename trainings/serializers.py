395928507963a937397189eeec94a48a440d067a
panjacob <s19615@pjwstk.edu.pl>
2021-10-26 15:25:34 +0200

    Added Files model for storing images in TrainingGroup

--- trainings/serializers.py
+++ trainings/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
-from trainings.models import TrainingGroup, TrainingGroupTypes
+from trainings.models import TrainingGroup, TrainingGroupTypes, Files
 
 
 class TrainingGroupTypesSerializer(ModelSerializer):
     model = TrainingGroupTypes
     fields = ['id']
+
+
+class FilesSerializer(ModelSerializer):
+    model = Files
+    training_group = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
+    owner = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
+    fields = ['my_file', 'owner']
 
 
 class TrainingGroupSerializerCreate(ModelSerializer):
