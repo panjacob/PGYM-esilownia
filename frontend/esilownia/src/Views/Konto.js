1a22ea1e8c5019ef7174c7e33a8aec2d6d19a9bc
C-h-e-s-h-i-r-e <aleksanderbober1999@gmail.com>
2021-10-26 20:18:13 +0200

    Dodanie PlaceHolder dla ProfilePhoto

--- frontend/esilownia/src/Views/Konto.js
+++ frontend/esilownia/src/Views/Konto.js
@@ -1,12 +1,15 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DaneKonto from '../components/Konto/Dane_konto'
+import ZdjecieKonto from "../components/Konto/Zdjecie_konto";
 
 function Konto() {
 
     return (
         <div className="konto">
             <div className="container">
+
+                <ZdjecieKonto></ZdjecieKonto>
 
                 <DaneKonto></DaneKonto>
 
