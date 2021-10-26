7528a5758c83d9387e616141a8a5d40091951be7
C-h-e-s-h-i-r-e <aleksanderbober1999@gmail.com>
2021-10-27 00:12:48 +0200

    Propozycja Kadra

--- frontend/esilownia/src/Views/Kadra.js
+++ frontend/esilownia/src/Views/Kadra.js
@@ -2,16 +2,36 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Karuzela_kadra from "../components/Kadra/Carousel_trenerzy";
 import Karuzela_dietetycy from "../components/Kadra/Carousel_dietetycy";
+import Trenerzy from "../components/Kadra/Trenerzy";
+import Dietetycy from "../components/Kadra/Dietetycy";
 
 function Kadra() {
     return (
         <div className="kadra">
             <div className="container pt-4 align-items-center text-center">
-                <h1 className="display-1">Poznaj naszą kadrę!</h1>
-                <h1 className="font-weight-light pt-4 pb-4">Nasi Trenerzy</h1>
+                <h1 className="display-1"><i>Poznaj naszą kadrę !</i></h1>
+
+                <hr></hr>
+                <h1 className="font-weight-light pt-4 pb-4"><i>Nasi Trenerzy</i></h1>
+                <hr></hr>
+                <Trenerzy></Trenerzy>
+                <Trenerzy></Trenerzy>
+                <hr></hr>
+
+                <h1 className="font-weight-light pt-4 pb-4">Wybierz z kim chcesz trenowac !</h1>
                 <Karuzela_kadra></Karuzela_kadra>
-                <h1 className="font-weight-light pt-4 pb-4">Nasi Dietetycy</h1>
+
+                <hr></hr>
+                <h1 className="font-weight-light pt-4 pb-4"><i>Nasi Dietetycy</i></h1>
+                <hr></hr>
+                <Dietetycy></Dietetycy>
+                <hr></hr>
+
+                <h1 className="font-weight-light pt-4 pb-4">Wybierz diete dostosowana do twojego treningu !</h1>
                 <Karuzela_dietetycy></Karuzela_dietetycy>
+
+
+
             </div>
         </div>
     );
