c777c1df8c2aab989a99fa4c15831187c9e8127e
Dudziej <dudzik32@gmail.com>
2021-10-26 20:58:40 +0200

    Kadra

--- frontend/esilownia/src/Views/Kadra.js
+++ frontend/esilownia/src/Views/Kadra.js
@@ -1,25 +1,17 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Karuzela_kadra from "../components/Kadra/Carousel_trenerzy";
+import Karuzela_dietetycy from "../components/Kadra/Carousel_dietetycy";
 
 function Kadra() {
     return (
         <div className="kadra">
-            <div className="container">
-                <div className="row align-items-center my-5">
-                    <div className="col-lg-7">
-                        <img
-                            className="img-fluid rounded mb-4 mb-lg-0"
-                            src="https://riotopgym.pl/wp-content/uploads/2020/06/IMG-3651.jpg"
-                            alt=""
-                        />
-                    </div>
-                    <div className="col-lg-5">
-                        <h1 className="font-weight-light">Kadra</h1>
-                        <p>
-                            Strona dla kadry ( trenerzy dietetycy itp.)
-                        </p>
-                    </div>
-                </div>
+            <div className="container pt-4 align-items-center text-center">
+                <h1 className="display-1">Poznaj naszą kadrę!</h1>
+                <h1 className="font-weight-light pt-4 pb-4">Nasi Trenerzy</h1>
+                <Karuzela_kadra></Karuzela_kadra>
+                <h1 className="font-weight-light pt-4 pb-4">Nasi Dietetycy</h1>
+                <Karuzela_dietetycy></Karuzela_dietetycy>
             </div>
         </div>
     );
---
+++ frontend/esilownia/src/components/Kadra/Carousel_dietetycy.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {Carousel} from "react-bootstrap";
+import {useState} from "react";
+import placeholder from "../../imgs/placeholder.jpg"
+
+function Karuzela_dietetycy() {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
+
+    return (
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={placeholder}
+                    alt="slide1"
+                />
+                <Carousel.Caption>
+                    <h3 className="font-weight-light">Jan Pudzianowski</h3>
+                    <p className="font-weight-light">Licencjownowany dietetyk, prowadzący Kulturystów.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={placeholder}
+                    alt="slide2"
+                />
+
+                <Carousel.Caption>
+                    <h3 className="font-weight-light">Paweł Karmowski</h3>
+                    <p className="font-weight-light">Dietetyk, pasjonata zdrowego żywienia.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    );
+}
+
+export default Karuzela_dietetycy;
