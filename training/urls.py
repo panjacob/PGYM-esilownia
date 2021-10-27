46165ab2319481e38f7728d169cd3f51e7d35498
panjacob <s19615@pjwstk.edu.pl>
2021-10-27 13:24:36 +0200

    Trainings url FIX

--- training/urls.py
+++ training/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from training import views
 
 urlpatterns = [
-    path('training/group/create', views.training_group_create, name='training_group_create'),
-    path('training/add_participant', views.training_group_add_participant, name='training_add_participant'),
-    path('training/remove_participant', views.training_group_remove_participant, name='training_remove_participant'),
+    path('group/create', views.training_group_create, name='training_group_create'),
+    path('add_participant', views.training_group_add_participant, name='training_add_participant'),
+    path('remove_participant', views.training_group_remove_participant, name='training_remove_participant'),
 ]
