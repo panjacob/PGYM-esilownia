398e1955a23df7a1f1e37e5a2919917681b63f05
panjacob <kwiatkowskijakub@protonmail.com>
2021-10-30 16:33:11 +0200

    Jitsi payload, is_training_owner

--- training/utilis.py
+++ training/utilis.py
@@ -1,4 +1,10 @@
+import time
+
 import jwt
+
+
+def current_milli_time():
+    return round(time.time() * 1000)
 
 
 def jitsi_token_encode(private_key, payload):
@@ -7,3 +13,33 @@ def jitsi_token_encode(private_key, payload):
 
 def jitsi_token_decode(public_key, token):
     return jwt.decode(token, public_key, algorithms=["RS256"])
+
+
+def jitsi_exp(time_to_live_minutes):
+    return current_milli_time() + time_to_live_minutes * 1000 * 60
+
+
+def jitsi_payload_create(user, training, time_to_live_minutes=10):
+    payload = {
+        'context': {
+            'user': {
+                'name': f"{user.first_name} {user.last_name}"
+            }
+        },
+        'moderator': is_training_owner(user, training),
+        'aud': 'pgym-jitsi',
+        'iss': 'pgym',
+        'sub': 'pgym-jitsi.xyz',
+        'room': training.id,
+        'exp': jitsi_exp(time_to_live_minutes)
+    }
+    return payload
+
+
+def is_training_owner(user, training):
+    return training.training_group.owner_id == user.id
+
+
+def is_training_group_owner(user, training_group):
+    pass
+
