81178d73d0c04470915a4670779ad8fc7aab1ef3
C-h-e-s-h-i-r-e <aleksanderbober1999@gmail.com>
2021-10-31 19:04:23 +0100

    Aktualizacja widoku O_Nas

--- frontend/esilownia/src/Views/O_nas.js
+++ frontend/esilownia/src/Views/O_nas.js
@@ -1,40 +1,19 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CechyONas from "../components/O_nas/Cechy_o_nas";
+import OpisONas from "../components/O_nas/Opis_o_nas";
 
 function O_nas() {
-  return (
-    <div className="o_nas pb-5">
-        <h1 className="display-1 text-center"> O nas</h1>
-        <div class="container text-center border radius mt-4 mb-4 shadow" id="linia_nas">
-          <h1 class="font-weight-light">Jaki jest cel aplikacji?</h1>
-            <div class="row mt-4">
-              <div class="col-lg-8">
-                <img
-                  class="img-fluid rounded mb-4 mb-lg-4"
-                  src="https://wallpapercave.com/wp/wp5559316.jpg"
-                  alt=""
-                  />
-                </div>
-                <div class="col-lg-4 font-weight-light">
-                  <h4 class="font-weight-light">Cel</h4>
-                    <ul>
-                      <li>
-                        Aplikacja która wspiera ludzi nie chcący, nie mogący pójść samemu na siłownię.
-                      </li>
-                      <li>
-                        Zachęcenie ludzi to zdrowego trybu życia.
-                      </li>
-                      <li>
-                        Umożliwienie trenerom pracy online na własnych warunkach.
-                      </li>
-                    </ul>
-                    <h4 class="font-weight-light">Kim jesteśmy?</h4>
-                    <p>Jesteśmy studentami 4-tego roku na Polsko Japońskiej Akademii Technik Komputerowych w Gdańsku.</p>
-                </div>
-              </div>
+    return (
+        <div className="o_nas pb-5">
+            <div class="container" >
+                <OpisONas></OpisONas>
+            </div>
+            <div className="container">
+                <CechyONas></CechyONas>
+            </div>
         </div>
-      </div>
-  );
+    );
 }
 
 export default O_nas;
