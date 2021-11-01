a05c9daea49c125244f583e733bae79fb534b1f2
C-h-e-s-h-i-r-e <aleksanderbober1999@gmail.com>
2021-11-01 21:26:14 +0100

    Facebook Login Base (problem z tybem developerskim)

--- frontend/esilownia/src/Views/Login.js
+++ frontend/esilownia/src/Views/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginForm from "../components/Login/Login_form"
+import LoginFb from "../components/Login/Login_Fb"
 
 function Login() {
 
@@ -9,6 +10,9 @@ function Login() {
             <div className="container">
                 <div className="row d-flex justify-content-center my-5">
                     <LoginForm></LoginForm>
+                </div>
+                <div className="row d-flex justify-content-center my-5">
+                    <LoginFb></LoginFb>
                 </div>
             </div>
         </div>
--- frontend/esilownia/src/Views/O_nas.js
+++ frontend/esilownia/src/Views/O_nas.js
@@ -6,7 +6,7 @@ import OpisONas from "../components/O_nas/Opis_o_nas";
 function O_nas() {
     return (
         <div className="o_nas pb-5">
-            <div class="container" >
+            <div className="container" >
                 <OpisONas></OpisONas>
             </div>
             <div className="container">
