6614a2ff711244e43f9d3b38f98469b530831a2c
Jakub Kwiatkowski <44145413+panjacob@users.noreply.github.com>
2021-11-05 17:54:21 +0100

    API: moderator/application/get and moderator/application/all

--- moderator/urls.py
+++ moderator/urls.py
@@ -4,5 +4,7 @@ from moderator import views
 
 urlpatterns = [
     path('application/send', views.application_send, name='moderator_application_send'),
+    path('application/get', views.application_get, name='moderator_application_get'),
+    path('application/all', views.application_all, name='moderator_application_all'),
     # path('user_day/get_all', views.user_day_get_all, name='user_day_get_all'),
 ]
--- moderator/views.py
+++ moderator/views.py
@@ -1,9 +1,11 @@
 # Create your views here.
+from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from moderator.serializers import ApplicationCreateSerializer
+from moderator import models
+from moderator.serializers import ApplicationCreateSerializer, ApplicationGetSerializer
 from users.utilis import put_owner_in_request_data
 
 
@@ -16,3 +18,21 @@ def application_send(request):
         if serializer.save():
             return Response({'id': serializer.instance.id}, status=status.HTTP_200_OK)
     return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def application_get(request):
+    application = models.Application.objects.get(id=request.data['id'])
+    serializer = ApplicationGetSerializer(application)
+    return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii': False})
+
+
+@api_view(['GET'])
+def application_all(request):
+    applications = models.Application.objects.all()
+    result = []
+    for application in applications:
+        serializer = ApplicationGetSerializer(application)
+        result.append(serializer.data)
+
+    return JsonResponse(result, safe=False, json_dumps_params={'ensure_ascii': False})
