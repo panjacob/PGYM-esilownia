ad93b3fb25a1a0e296ce911de7d64b42982f9ba4
Jakub Kwiatkowski <44145413+panjacob@users.noreply.github.com>
2021-11-09 09:42:43 +0100

    API: dashboard/achievment/user/remove

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('achievment/all', views.achievment_all, name='achievment_all'),
     path('achievment/remove', views.achievment_remove, name='achievment_remove'),
     path('achievment/user/add', views.achievment_user_add, name='achievment_user_add'),
+    path('achievment/user/remove', views.achievment_user_remove, name='achievment_user_remove'),
 ]
