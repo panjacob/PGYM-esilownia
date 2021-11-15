bceb5d14ebf046e7f9f5e2050be870360fd2fbcc
panjacob <s19615@pjwstk.edu.pl>
2021-11-15 10:06:40 +0100

    notification_send() funnction

--- message/models.py
+++ message/models.py
@@ -24,7 +24,7 @@ class Notification(models.Model):
         (APPLICATION_REJECTED, 'APPLICATION REJECTED'),
     ]
 
-    type = models.CharField(max_length=1, choices=TYPE_CHOICES, default=None)
+    kind = models.CharField(max_length=1, choices=TYPE_CHOICES, default=None)
 
     body = models.CharField(max_length=10000)
     time = models.IntegerField(default=current_milli_time)
--- message/utilis.py
+++ message/utilis.py
@@ -1,9 +1,14 @@
 from django.db.models import Q
 
-from message.models import Message
+from message.models import Message, Notification
 
 
 def get_messages_all(user1, user2):
     messages = Message.objects.filter((Q(sender=user1) | Q(sender=user2) | Q(receiver=user1) | Q(receiver=user2)))
     messages = messages.order_by('time')
     return messages
+
+
+def notification_send(user, body, kind):
+    notification = Notification.objects.create(user=user, body=body, kind=kind)
+    return notification
