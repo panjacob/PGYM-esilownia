20c5e9e9a3d0799339d4d7e6188e140ccf93dc76
panjacob <s19615@pjwstk.edu.pl>
2021-11-16 15:15:11 +0100

    Payment - Transaction in progress

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,20 @@
+import transaction as transaction
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+from payment.models import Transaction, Offer
+
+
+@transaction.atomic
+def transaction_create(request):
+    user = request.user
+    offer_id = request.data['offer']
+    offer = Offer.objects.get(id=offer_id)
+
+    transaction = Transaction.objects.create(user=user, payed=offer.price, purchased=offer.coins)
+    user.coins += offer.coins
+    user.save()
+    transaction.save()
+
+    return Response({'OK'}, status=status.HTTP_200_OK)
