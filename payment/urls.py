f77b783d59e2654df98c2def1c9b09561fadf023
panjacob <s19615@pjwstk.edu.pl>
2021-11-17 10:48:10 +0100

    API payment/offer/all

--- payment/urls.py
+++ payment/urls.py
@@ -5,5 +5,6 @@ from payment import views
 urlpatterns = [
     path('transaction/create', views.transaction_create, name='transaction_create'),
     path('transaction/all', views.transaction_all, name='transaction_all'),
+    path('offer/all', views.offer_all, name='offer_all'),
 
 ]
--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,8 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from payment import utilis
 from payment import models
@@ -27,10 +28,11 @@ def transaction_all(request):
 
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def offer_all(request):
     offers = models.Offer.objects.all()
     result = []
     for offer in offers:
-        serializer = serializers.TransactionSerializer(instance=offer)
+        serializer = serializers.OfferSerializer(instance=offer)
         result.append(serializer.data)
     return JsonResponse(result, safe=False)
