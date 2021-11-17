ab9280a0e691ea23a42cf8a9eff5a1f6a9d7d399
panjacob <s19615@pjwstk.edu.pl>
2021-11-17 10:16:37 +0100

    Offer and Transaction model fixes

--- payment/models.py
+++ payment/models.py
@@ -5,14 +5,14 @@ from payment.utilis import random_string
 
 
 class Transaction(models.Model):
-    transaction_id = models.CharField(unique=True, default=random_string)
+    transaction_id = models.CharField(unique=True, default=random_string, max_length=50)
     user = models.ForeignKey(UserExtended, on_delete=models.CASCADE)
     time = models.IntegerField(default=current_milli_time)
-    payed = models.DecimalField(decimal_places=2)
+    payed = models.DecimalField(decimal_places=2, max_digits=10)
     purchased = models.IntegerField()
 
 
 class Offer(models.Model):
-    name = models.CharField()
-    price = models.DecimalField(decimal_places=2)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
     coins = models.IntegerField()
