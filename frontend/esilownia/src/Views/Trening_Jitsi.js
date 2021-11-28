84793799c2968e37495f6bfe495a22e3358a70fd
Dudziej <dudzik32@gmail.com>
2021-11-28 16:42:17 +0100

    Usuwanie Grup + guzik wstecz w Training_trainer.js, Wstępny zarys widoku Trening_Jitsi.js

--- frontend/esilownia/src/Views/Trening_Jitsi.js
+++ frontend/esilownia/src/Views/Trening_Jitsi.js
@@ -3,17 +3,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {useLocation} from "react-router-dom";
 import axiosInstance from "../components/Axios/Axios";
 
-function TrainingJitsi() {
-
+function TrainingJitsi(props) {
+    const [groupId, setGroupId] = useState("")
     const [trainingInfo, setTrainingInfo] = useState([])
 
     const location = useLocation()
+
+    useEffect(() => {
+
+        setGroupId(location.state.trainingId)
+
+        axiosInstance
+            .post(`training/get`, {id: groupId}, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': localStorage.getItem('token_type') + ' ' + localStorage.getItem('access_token')
+                }
+            })
+            .then((res) => {
+                setTrainingInfo(res.data)
+            });
+
+    }, [props.groupId]);
 
     const jitsiStart = (jwt) => {
         const roomName = location.state.trainingId;
         const domain = "meet.pgym.xyz";
         const options = {
-            width: 1200,
+            width: 1100,
             height: 700,
             parentNode: document.querySelector('#meet'),
             roomName: roomName,
@@ -47,9 +64,40 @@ function TrainingJitsi() {
     };
 
     return (
-        <div className="treningJitsi">
-            <div className="container mt-4">
-                <div id='meet'></div>
+        <div className="treningJitsi justify-content-center">
+            <div className="container">
+                <div className="text-center">
+                    <hr></hr>
+                    <h1 style={{"fontSize": "5vw"}} className="display-1 font-weight-light mb-4">{trainingInfo.title}
+                    </h1>
+                    <hr></hr>
+                </div>
+                <div className="container mt-4 border" style={{'width':'1100px', 'height':'700px'}} >
+                    <div id='meet'></div>
+                </div>
+                <div className="card mb-4 mt-4 bg-light" width="100%">
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col-sm-5">
+                                <h6 className="mb-0">Opis</h6>
+                            </div>
+                            <div className="col-sm-7 text-secondary">
+                                {trainingInfo.description}
+                            </div>
+                        </div>
+                        <hr/>
+                        <div className="row">
+                            <div className="col-sm-5">
+                                <h6 className="mb-0">Czas Trwania</h6>
+                            </div>
+                            <div className="col-sm-7 text-secondary">
+                                {trainingInfo.date_start} - {trainingInfo.date_end}
+                            </div>
+                        </div>
+                        <hr/>
+                    </div>
+
+                </div>
             </div>
         </div>
     );
