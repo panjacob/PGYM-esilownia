419bb70d9e97302a629160de15a4861dcfd5bf68
panjacob <s19615@pjwstk.edu.pl>
2021-11-29 10:52:56 +0100

    API: dashboard/user_day/get - added new endpoint to get user_day by each day

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,6 +5,7 @@ from dashboard import views
 urlpatterns = [
     path('user_day/create', views.user_day_create, name='user_day_create'),
     path('user_day/get_all', views.user_day_get_all, name='user_day_get_all'),
+    path('user_day/get', views.user_day_get, name='user_day_get'),
     path('achievment/create', views.achievment_create, name='achievment_create'),
     path('achievment/get', views.achievment_get, name='achievment_get'),
     path('achievment/all', views.achievment_all, name='achievment_all'),
