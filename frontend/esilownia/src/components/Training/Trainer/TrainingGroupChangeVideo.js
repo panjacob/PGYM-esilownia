6a86e90b7de7ad4919f547e9f2ee5c327645d066
Dudziej <dudzik32@gmail.com>
2021-12-02 18:12:19 +0100

    TrainingGroupChangeVideo.js - W pełni działające dodawanie, usuwanie i wyświetlanie Filmów

--- frontend/esilownia/src/components/Training/Trainer/TrainingGroupChangeVideo.js
+++ frontend/esilownia/src/components/Training/Trainer/TrainingGroupChangeVideo.js
@@ -6,9 +6,12 @@ import axios_variebles from "../../Axios/Axios_variebles";
 function TrainingGroupChangeVideo(props){
     const [groupInfo, setGroupInfo] = useState([]);
     const [video, setVideo] = useState();
+    const [groupInfoVideos, setGroupInfoVideos] = useState([]);
+    const [videoSelected, setVideoSelected] = useState('none');
     const [fileToUpload, setFileToUpload] = useState();
     const [fileToUploadName, setFileToUploadName] = useState("");
     const [isFilePicked, setIsFilePicked] = useState(false);
+    const [videoSelectedName, setVideoSelectedName] = useState("");
 
     useEffect(() => {
 
@@ -21,6 +24,7 @@ function TrainingGroupChangeVideo(props){
             })
             .then((res) => {
                 setGroupInfo(res.data)
+                setGroupInfoVideos(res.data.videos)
             });
 
     }, [props.groupId]);
@@ -38,23 +42,12 @@ function TrainingGroupChangeVideo(props){
         var myHeaders = new Headers();
         myHeaders.append("Authorization", localStorage.getItem('token_type') + ' ' + localStorage.getItem('access_token'));
 
-        var formdata2 = new FormData();
-        formdata2.append("id", groupInfo.id);
-
-        var requestOptions2 = {
-            method: 'POST',
-            headers: myHeaders,
-            body: formdata2,
-            redirect: 'follow'
-        };
-
-        fetch(axios_variebles.baseURL + "training/group/video/remove", requestOptions2)
-            .then(response => response.text())
-            .then(result => console.log(result))
-            .catch(error => console.log('error', error));
 
         var formdata = new FormData();
-        formdata.append("video", fileToUpload, fileToUploadName);
+        if(isFilePicked===true){
+            formdata.append("video", fileToUpload, fileToUploadName);
+        }
+        formdata.append("training_group", groupInfo.id )
 
         var requestOptions = {
             method: 'POST',
@@ -71,42 +64,81 @@ function TrainingGroupChangeVideo(props){
         window.location.reload()
 
     }
+
+    const videoChosen = (e) => {
+        if(e.target.value !=='none'){
+            setVideoSelected(e.target.value)
+            groupInfoVideos.map((video)=>{
+                if(video.id.toString() === e.target.value){
+                    setVideoSelectedName(video.url)
+                }
+            })
+        }else{
+            setVideoSelectedName( 'none')
+            setVideoSelected('none')
+        }
+    }
+
+
+    function validateForm() {
+        return videoSelected !== 'none';
+    }
+
+    const handleRemoveVid = (e) => {
+        e.preventDefault();
+
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", localStorage.getItem('token_type') + ' ' + localStorage.getItem('access_token'));
+
+
+        var formdata2 = new FormData();
+        formdata2.append("id", videoSelected);
+
+
+        var requestOptions2 = {
+            method: 'POST',
+            headers: myHeaders,
+            body: formdata2,
+            redirect: 'follow'
+        };
+
+        fetch(axios_variebles.baseURL + "training/group/video/remove", requestOptions2)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+
+        window.location.reload()
+
+    }
+
+
     return(
-        <div className="TrainingGroupChangeImage">
+        <div className="TrainingGroupChangeVideo">
             <hr/>
-            <h1 style={{"fontSize": "4vw"}} className="display-1 font-weight-light mb-4">Zmień Film Grupy</h1>
+            <h1 style={{"fontSize": "4vw"}} className="display-1 font-weight-light mb-4">Zmień Filmy Instruktażowe</h1>
             <hr/>
             <div className="col-md-8 mx-auto mt-3">
                 <div className="card mb-3 bg-light">
                     <div className="card-body">
                         <div className="row">
-                            <div className="mx-auto">
-                                <h6 className="mb-0">Film Grupy</h6>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="mx-auto">
-                                <img src={video} alt="..." className="img-thumbnail" width='200px'
-                                     height='200px'/>
-                            </div>
-                        </div>
-                        <div className="row">
                             <div className="mx-auto pt-1">
                                 <div className="custom-file">
-                                    <input type="file" accept="video/*" className="custom-file-input" id="customFile" onChange={onFileChange}></input>
+                                    <input type="file" accept="video/*"
+                                           className="custom-file-input" id="customFile" onChange={onFileChange}>
+                                    </input>
                                     <label className="custom-file-label" htmlFor="customFile">Wybierz plik</label>
                                     {isFilePicked ? (
                                         <div>
-                                            <p>Filename: {fileToUpload.name}</p>
-                                            <p>Filetype: {fileToUpload.type}</p>
-                                            <p>Size in bytes: {fileToUpload.size}</p>
+                                            <p>Nazwa: {fileToUpload.name}</p>
+                                            <p>Typ: {fileToUpload.type}</p>
+                                            <p>Rozmiar: {fileToUpload.size}</p>
                                             <p>
-                                                lastModifiedDate:{' '}
+                                                Ostatnia modyfikacja:{' '}
                                                 {fileToUpload.lastModifiedDate.toLocaleDateString()}
                                             </p>
                                         </div>
                                     ) : (
-                                        <p>Select a file to show details</p>
+                                        <p>Wybierz plik aby zobaczyć szczegóły</p>
                                     )}
                                 </div>
                             </div>
@@ -115,11 +147,40 @@ function TrainingGroupChangeVideo(props){
 
                         <div className="row">
                             <div className="col-sm-3">
-                                <Button onClick={handleSubmitVid} variant="btn" size="sm">Zmien Film</Button>
+                                <Button onClick={handleSubmitVid} variant="btn" size="sm">Dodaj Film</Button>
                             </div>
+                            <div className="col-sm-3">
+                                <p className="font-weight-bold">wybierz film do usunięcia</p>
+                            </div>
+                            <div className='col-sm-3'>
+                                <select className='text-center' style={{width: '100%', height: '30px'}}
+                                        onChange={videoChosen}>
+                                    <option value='none'> - </option>
+                                    {groupInfoVideos.map(function (videos, idx) {
+                                        return (
+                                            <option
+                                                key={idx}
+                                                value={videos.id}
+                                                name={(videos.url).toString()}
+                                            >
+                                                {(videos.url).split("/").pop()}
+                                            </option>
+                                        )
+                                    })}
+                                </select>
+                            </div>
+                            <div className="col-sm-3">
+                                <Button onClick={handleRemoveVid} variant="btn" size="sm" disabled={!validateForm()}>Usuń Film</Button>
+                            </div>
+
                         </div>
                     </div>
+
                 </div>
+                <div className="container mt-4 mb-4">
+                    <video className="img-thumbnail" src={axios_variebles.baseURL.slice(0, -1) + videoSelectedName} controls/>
+                </div>
+
             </div>
         </div>
     );
