f6c9dfa86ea182440af4394a5f87e59970580b3a
Dudziej <dudzik32@gmail.com>
2021-12-06 19:46:13 +0100

    Wykresy UserDay

--- frontend/esilownia/src/Views/Dashboard.js
+++ frontend/esilownia/src/Views/Dashboard.js
@@ -5,6 +5,7 @@ import DaneUser from "../components/Dashboard/Dashboard_data_user";
 import DashboardApplication from "../components/Dashboard/Dashboard_application";
 import OfertaTrener from "../components/Dashboard/Dashboard_offer_trainer";
 import {Button, Nav} from "react-bootstrap";
+import Dashboard_chart from "../components/Dashboard/Dashboard_chart";
 
 function Dashboard() {
 
@@ -36,6 +37,7 @@ function Dashboard() {
                     </div>
                 </div>
                 <DaneUserDay></DaneUserDay>
+                <Dashboard_chart></Dashboard_chart>
                 <div className="container text-center">
                 <h1 style={{"fontSize": "5vw"}} className="display-1 pb-4 pt-4">Jesteś trenerem?</h1>
                 <h4 style={{"fontSize": "2vw"}} className="font-weight-light pb-4">Dołącz do naszego zespołu!</h4>
