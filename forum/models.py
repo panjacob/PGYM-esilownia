ce20845dd77b1db2ad47057b7791053c64c35a2e
panjacob <s19615@pjwstk.edu.pl>
2021-12-06 10:04:23 +0100

    Forum: Changed DateField to DatetimeField in Post and Topic

--- forum/models.py
+++ forum/models.py
@@ -8,13 +8,13 @@ from users.models import UserExtended
 
 class Post(models.Model):
     owner = models.ForeignKey(UserExtended, on_delete=models.CASCADE, null=True, default=None)
-    date = models.DateField(default=timezone.now)
+    date = models.DateTimeField(default=timezone.now)
     body = models.CharField(max_length=100000)
 
 
 class Topic(models.Model):
     owner = models.ForeignKey(UserExtended, on_delete=models.CASCADE, null=True, default=None)
-    date = models.DateField(default=timezone.now)
+    date = models.DateTimeField(default=timezone.now)
     title = models.CharField(max_length=1000)
     body = models.CharField(max_length=100000)
     posts = models.ManyToManyField(Post)
