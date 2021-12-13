459b934d479ca868f803aa6b142002771d206cc4
panjacob <s19615@pjwstk.edu.pl>
2021-12-13 09:41:46 +0100

    DietGroupType init

--- diet/apps.py
+++ diet/apps.py
@@ -4,3 +4,24 @@ from django.apps import AppConfig
 class DietConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'diet'
+
+    def ready(self):
+        import os
+        if os.environ.get('RUN_MAIN', None) != 'true':
+            init_DietGroupTypes()
+
+            # from training.utilis import remove_participant_from_training_group_when_subscription_end, \
+            #     do_job_every_x_seconds
+            # do_job_every_x_seconds(remove_participant_from_training_group_when_subscription_end, 1 * 60 * 60)
+
+
+def init_DietGroupTypes():
+    try:
+        from diet.models import DietGroupType, DietGroupTypes
+        for id, type in DietGroupTypes:
+            instance, created = DietGroupType.objects.get_or_create(id=id)
+            instance.type = type
+            instance.save()
+
+    except Exception as e:
+        pass
