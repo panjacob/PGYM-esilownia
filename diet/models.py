8ab592b6b71fd5338ecc12330e373f305edfe331
panjacob <s19615@pjwstk.edu.pl>
2021-12-13 09:41:32 +0100

    Diet models.py

--- diet/models.py
+++ diet/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+from users.models import UserExtended
+
+DietGroupTypes = [
+    (1, 'podstawowa'),
+    (2, 'łatwo strawna'),
+    (3, 'wysokobiałkowa'),
+    (4, 'niskobiałkowa'),
+    (5, 'bezglutenowa'),
+    (6, 'cukrzycowa'),
+    (7, 'ubogoenergetyczna'),
+    (8, 'o ograniczeniem tłuszczu'),
+    (9, 'dieta wrzodowa'),
+    (10, 'bogato resztkowa'),
+    (20, 'inna'),
+]
+
+
+class DietGroupType(models.Model):
+    type = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=300)
+
+
+class DietFile(models.Model):
+    file = models.FileField(null=True, blank=True)
+
+
+class DietGroup(models.Model):
+    owner = models.ForeignKey(UserExtended, on_delete=models.CASCADE, null=True, default=None,
+                              related_name='owner_diet')
+    date = models.DateField(default=timezone.now)
+    is_private = models.BooleanField(default=False)
+    type = models.ManyToManyField(DietGroupType)
+    title = models.CharField(max_length=300)
+    description = models.CharField(max_length=10000)
+    price_day = models.IntegerField(default=None, null=True)
+    price_week = models.IntegerField(default=None, null=True)
+    price_month = models.IntegerField(default=None, null=True)
+
+    image = models.ImageField(null=True, blank=True)
+    files = models.ManyToManyField(DietFile)
