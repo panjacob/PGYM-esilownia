f2224439cafd1d77f4873d507226dcf9591871e3
panjacob <s19615@pjwstk.edu.pl>
2021-12-14 14:20:48 +0100

    Diet remove 'group' from url

--- diet/urls.py
+++ diet/urls.py
@@ -3,16 +3,20 @@ from django.urls import path
 from diet import views
 
 urlpatterns = [
-    path('group/create', views.diet_group_create, name='diet_group_create'),
-    path('group/edit', views.diet_group_edit, name='diet_group_edit'),
-    path('group/get', views.diet_group_get, name='diet_group_get'),
-    path('group/all', views.diet_group_all, name='diet_group_all'),
-    path('group/remove', views.diet_group_remove, name='diet_group_remove'),
-    path('group/participant/join', views.diet_group_join, name='diet_group_join'),
-    path('group/participant/remove', views.diet_group_participant_remove, name='diet_group_participant_remove'),
-    path('group/type/get', views.diet_group_type_get, name='diet_group_type_get'),
-    path('group/type/all', views.diet_group_type_all, name='diet_group_type_all'),
+    path('create', views.diet_group_create, name='diet_group_create'),
+    path('edit', views.diet_group_edit, name='diet_group_edit'),
+    path('get', views.diet_group_get, name='diet_group_get'),
+    path('all', views.diet_group_all, name='diet_group_all'),
+    path('remove', views.diet_group_remove, name='diet_group_remove'),
+    path('participant/join', views.diet_group_join, name='diet_group_join'),
+    path('participant/remove', views.diet_group_participant_remove, name='diet_group_participant_remove'),
+    path('type/get', views.diet_group_type_get, name='diet_group_type_get'),
+    path('type/all', views.diet_group_type_all, name='diet_group_type_all'),
 
-    path('group/file/add', views.diet_group_file_add, name='diet_group_file_add'),
-    path('group/file/remove', views.diet_group_file_remove, name='diet_group_file_remove'),
+    path('file/add', views.diet_group_file_add, name='diet_group_file_add'),
+    path('file/remove', views.diet_group_file_remove, name='diet_group_file_remove'),
+
+    path('remove', views.diet_group_file_remove, name='diet_group_file_remove'),
+
+
 ]
