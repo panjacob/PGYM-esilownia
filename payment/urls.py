a39bdc7c6166311ac24b7e1c3fa7b2e8415ec06c
panjacob <s19615@pjwstk.edu.pl>
2021-12-17 11:18:53 +0100

    API: payment/transaction/create and payment/transaction/all removed

--- payment/urls.py
+++ payment/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from payment import views
 
 urlpatterns = [
-    path('transaction/create', views.transaction_create, name='transaction_create'),
-    path('transaction/all', views.transaction_all, name='transaction_all'),
+    # path('transaction/create', views.transaction_create, name='transaction_create'),
+    # path('transaction/all', views.transaction_all, name='transaction_all'),
     path('offer/all', views.offer_all, name='offer_all'),
     path('stripepk', views.stripepk, name='stripepk'),
     path('create_checkout_session', views.create_checkout_session, name='create_checkout_session'),
