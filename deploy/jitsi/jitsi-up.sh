17f7857007c4726613bf911894a201b6aa245989
PRMTRG <trgprm@gmail.com>
2022-01-07 18:41:08 +0100

    Update deploy files

--- deploy/jitsi/jitsi-up.sh
+++ deploy/jitsi/jitsi-up.sh
@@ -4,3 +4,5 @@ set -e
 
 docker-compose -f docker-compose.yml -f jibri.yml up -d --scale jibri=5
 ./jibri-scale.sh
+sed -i 's/SHOW_JITSI_WATERMARK: true/SHOW_JITSI_WATERMARK: false/g' config/web/interface_config.js
+
